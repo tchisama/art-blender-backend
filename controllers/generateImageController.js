fa13ab9827772ee97bd728acb4d6157bf85304b7
tchi-sama <tchisama41@gmail.com>
2023-11-11 02:11:36 -0800

    adding a test var

--- controllers/generateImageController.js
+++ controllers/generateImageController.js
@@ -4,31 +4,36 @@ const { openai } = require('../config/openAi');
 const generateImageService = require('../services/generateImageService');
 const axios = require('axios');
 const fs = require('fs');
-
+const test = true
 exports.generateImage = async (req, res) => {
     const { prompt , n , size , model,dallev } = req.body;
     try{
-        const image = await openai.images.generate({ 
-            model: dallev, 
-            prompt: modules[model].prompt.replace(/PROMPT/g, prompt), 
-            n: n,
-            size: size
-        });
+        var image;
+        if(test){
+          image = {
+              data:[
+                  {
+                    revised_prompt: 'CCreate an illustration inspired by the vibrant energy of Japanese manga, featuring a charming feline with soft fur and sparkling eyes cheerfully leaping to engage in playful dynamics with a lively avian creature. The scene is set against a backdrop of an enchanting forest brimming with lush foliage. In the air, soft pink sakura petals are gently swirling, adding to the ethereal atmosphere. The cat and the bird are showing a playful camaraderie, their actions filled with mutual respect and care, capturing a beautiful interaction often seen in friendships of unusual pairs.reate an illustration inspired by the vibrant energy of Japanese manga, featuring a charming feline with soft fur and sparkling eyes cheerfully leaping to engage in playful dynamics with a lively avian creature. The scene is set against a backdrop of an enchanting forest brimming with lush foliage. In the air, soft pink sakura petals are gently swirling, adding to the ethereal atmosphere. The cat and the bird are showing a playful camaraderie, their actions filled with mutual respect and care, capturing a beautiful interaction often seen in friendships of unusual pairs.',
+                    url: 'https://firebasestorage.googleapis.com/v0/b/alamiphotography-b75a1.appspot.com/o/img-wPP4vtqYZf6qOnJVM5ABFOHa.png?alt=media&token=e617b941-c62b-43e8-9036-9f446138541e'    
+                  },
+                ]
+          }
+        }else{
 
+          image = await openai.images.generate({ 
+              model: dallev, 
+              prompt: modules[model].prompt.replace(/PROMPT/g, prompt), 
+              n: n,
+              size: size
+          });
+
+          image.data.forEach(element => {
+              downloadImage(element.url,"./demo/"+Math.floor(Math.random()*10000000)+".png");
+          });
+        }
         // // console.log(image.data);
-        // const image = {
-        //     data:[
-        //         {
-        //           revised_prompt: 'CCreate an illustration inspired by the vibrant energy of Japanese manga, featuring a charming feline with soft fur and sparkling eyes cheerfully leaping to engage in playful dynamics with a lively avian creature. The scene is set against a backdrop of an enchanting forest brimming with lush foliage. In the air, soft pink sakura petals are gently swirling, adding to the ethereal atmosphere. The cat and the bird are showing a playful camaraderie, their actions filled with mutual respect and care, capturing a beautiful interaction often seen in friendships of unusual pairs.reate an illustration inspired by the vibrant energy of Japanese manga, featuring a charming feline with soft fur and sparkling eyes cheerfully leaping to engage in playful dynamics with a lively avian creature. The scene is set against a backdrop of an enchanting forest brimming with lush foliage. In the air, soft pink sakura petals are gently swirling, adding to the ethereal atmosphere. The cat and the bird are showing a playful camaraderie, their actions filled with mutual respect and care, capturing a beautiful interaction often seen in friendships of unusual pairs.',
-        //           url: 'https://firebasestorage.googleapis.com/v0/b/alamiphotography-b75a1.appspot.com/o/img-wPP4vtqYZf6qOnJVM5ABFOHa.png?alt=media&token=e617b941-c62b-43e8-9036-9f446138541e'    
-        //         },
-        //       ]
-        // }
         
         
-        image.data.forEach(element => {
-            downloadImage(element.url,"./demo/"+Math.floor(Math.random()*10000000)+".png");
-        });
 
         res.json(
             {
